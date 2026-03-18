bb19252144222a555ca6ea81ef62c0322c4cc50a
Myrza Mukanbetov <mukanbetovma07@gmail.com>
2026-03-18 20:35:53 +0600

    Update main.cpp and README.md, add helper.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
-int main() {return 0;}
-//second change
-//hotfix in master
+#include <iostream>
+using namespace std;
+
+int sum(int arr[], int size) {
+    int s = 0;
+    for(int i = 0; i < size; i++) {
+        s += arr[i];
+    }
+    return s;
+}
+
+int main() {
+    cout << "Hello World" << endl;
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout << "Sum: " << sum(arr, 5) << endl;
+
+    return 0;
+}
